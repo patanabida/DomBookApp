a0158fa1f5a78e1e1834e1572bff0c602c1314a7
patanabiad <patanabida1106@gmail.com>
2024-12-28 16:47:07 +0530

    updates

--- scripts/admin.js
+++ scripts/admin.js
@@ -1,3 +1,16 @@
+import { baseUrl } from "./baseUrl";
+
+let form=document.getElementById("form");
+let addbook=document.getElementById("gridcont");
+
+//fetch the data
+let loadBook=async ()=>{
+    let res=await fetch("https://keen-abundant-thread.glitch.me");
+    let books=await res.json();
+     addbook.innerHTML="";
+     
+
+}
 
 
 
